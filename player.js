a66bf44fc5b70fa0161650eb023ca38037af0746
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-11-12 14:25:24 -0800

    more things

--- player.js
+++ player.js
@@ -24,15 +24,21 @@ class Player {
         break;
       case 'down':
         // increas y position
-        this.y += this.speed;
+        if (this.y < h - this.r) {
+          this.y += this.speed;
+        }
         break;
       case 'right':
         // increase x position
+          if (this.x < h - this.r) {
         this.x += this.speed;
+      }
         break;
       case 'left':
         // decrease x posititon
+          if (this.x < h + this.r) {
         this.x -= this.speed;
+      }
         break;
       default:
         break;
